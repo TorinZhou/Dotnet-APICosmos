09241b06a89c8fd5e8cf84e1c1b10d9beb3b1e36
Torin Zhou <torinzhou@gmail.com>
2024-01-11 11:36:47 +0800

    updated the connectionString

--- Intermediate/1-HelloWorld_Models/Program.cs
+++ Intermediate/1-HelloWorld_Models/Program.cs
@@ -1,17 +1,14 @@
 ﻿using Dapper;
-using HelloWorld.Models;
 using Microsoft.Data.SqlClient;
 
 namespace HelloWorld;
 
-
 internal class Program
 {
 
-
     static void Main(string[] args)
     {
-        string connectionString = "Server=localhost;Database=DotNetCourseDatabase;Trusted_Connection=true,TrustServerCertificate=true";
+        string connectionString = "Server=localhost;Database=DotNetCourseDatabase;Trusted_Connection=true;TrustServerCertificate=true";
 
         var dbConnection = new SqlConnection(connectionString);
 
@@ -21,20 +18,6 @@ internal class Program
 
         System.Console.WriteLine(result);
 
-        // Computer myComputer = new Computer()
-        // {
-        //     Motherboard = "Z690",
-        //     HasWifi = true,
-        //     HasLTE = false,
-        //     ReleaseDate = DateTime.Now,
-        //     Price = 943.87m,
-        //     VideoCard = "RTX 2060"
-        // };
-        // myComputer.HasWifi = false;
-        // Console.WriteLine(myComputer.Motherboard);
-        // Console.WriteLine(myComputer.HasWifi);
-        // Console.WriteLine(myComputer.ReleaseDate);
-        // Console.WriteLine(myComputer.VideoCard);
     }
 
 }
